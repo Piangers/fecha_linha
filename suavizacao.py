ff22286e9ec7b06a3a895441ed85cbfc8f209e3b
Piangers <condeczar@gmail.com>
2018-03-29 18:15:50 -0300

    Alteração 29/03/2018 simplificação do código v1

--- suavizacao.py
+++ suavizacao.py
@@ -1,37 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
-/***************************************************************************
- Suavizacao
-                                 A QGIS plugin
- ferramenta
-                              -------------------
-        begin                : 2018-03-07
-        git sha              : $Format:%H$
-        copyright            : (C) 2018 by piangers
-        email                : cesar_piangers@hotmail.com
- ***************************************************************************/
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-"""
 import math
 from qgis.core import QGis, QgsVectorLayer, QgsGeometry
 from PyQt4 import QtCore, QtGui
 from PyQt4.QtGui import QIcon, QAction
-#include <qgspointv2.h>
-#include <qgsgeometry.h>
-#include <qgsabstractgeometryv2.h>
-
-# Initialize Qt resources from file resources.py
 import resources_rc
-# Import the code for the dialog
-from suavizacao_dialog import SuavizacaoDialog
 import os.path
 from qgis.gui import QgsMessageBar
 
@@ -40,13 +13,7 @@ class Suavizacao:
     """QGIS Plugin Implementation."""
 
     def __init__(self, iface):
-        """Constructor.
-
-        :param iface: An interface instance that will be passed to this class
-            which provides the hook by which you can manipulate the QGIS
-            application at run time.
-        :type iface: QgisInterface
-        """
+        
         # Save reference to the QGIS interface
         self.iface = iface
         # initialize plugin directory
@@ -70,47 +37,7 @@ class Suavizacao:
         status_tip=None,
         whats_this=None,
         parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        # Create the dialog (after translation) and keep reference
-        self.dlg = SuavizacaoDialog()
+        
 
         icon = QIcon(icon_path)
         action = QAction(icon, text, parent)
